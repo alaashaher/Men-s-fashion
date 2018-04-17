da5bb8cac00bbc58eefd4b1d42391f0aa419bb34
الاء شاهر <30909733+alaashaher@users.noreply.github.com>
2018-04-17 23:32:10 +0200

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -13,16 +13,3 @@ function opencours(evt, cothestype) {
 }
 document.getElementById("defaultOpen").click();
 
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 30.045915,lng: 31.224290},
-          zoom: 13
-        });
-        var tribeca = {lat: 30.045915,lng: 31.224290};
-        var marker = new google.maps.Marker({
-          position: tribeca,
-          map: map,
-          title: 'First Marker!'
-        });
-      }
